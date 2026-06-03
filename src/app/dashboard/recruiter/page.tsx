cd9f3f043ceb5ef6e03193b01e75379ffcb59677
omarFarukGit <muhammadabuhafz@gmail.com>
2026-06-03 22:23:56 +0600

    get user data recruiterpage

--- src/app/dashboard/recruiter/page.tsx
+++ src/app/dashboard/recruiter/page.tsx
@@ -1,12 +1,13 @@
+"use client";
 import { DashBoardSideBar } from "@/components/dashboard/DashBoardSideBar";
+import { useSession } from "@/lib/auth-client";
 import React from "react";
 
-const RecruiterPage = () => {
-  return (
-    <div>
-     i ame reqter
-    </div>
-  );
+const DashBoardRecruiterPage = () => {
+  const { data: session, isPending } = useSession();
+  console.log("re", session);
+
+  return <div>i ame reqter</div>;
 };
 
-export default RecruiterPage;
+export default DashBoardRecruiterPage;
