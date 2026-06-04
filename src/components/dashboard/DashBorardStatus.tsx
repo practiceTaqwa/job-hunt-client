fb53770ee0ba6b19b3960c2e6b5632a443d69660
omarFarukGit <muhammadabuhafz@gmail.com>
2026-06-04 20:37:33 +0600

    dashbaord Status componentes

---
+++ src/components/dashboard/DashBorardStatus.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DashBorardStatus = () => {
+    return (
+        <div>
+            DashBaordStatus
+        </div>
+    );
+};
+
+export default DashBorardStatus;
