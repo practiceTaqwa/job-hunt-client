8a5dcdf1429126687a39d21210c5d47f54047b30
practiceTaqwa <muhammadabuhafz@gmail.com>
2026-06-13 19:50:22 +0600

    addmin aproved fuctionality added

--- src/app/dashboard/admin/companies/page.tsx
+++ src/app/dashboard/admin/companies/page.tsx
@@ -1,9 +1,14 @@
+import CompanyTable from '@/components/dashboard/CompanyTable';
 import React from 'react';
 
-const AddminCompaniesPage = () => {
+const AddminCompaniesPage =async () => {
+    const res=await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/company/allcompany`).then((res)=>res.json())
+
+    const companies=res.data
     return (
         <div>
-            <h2>companies for reviews</h2>
+            <h2>companies for reviews {companies.length}</h2>
+            <CompanyTable companies={companies}></CompanyTable>
         </div>
     );
 };
